1f7e4b942c8bc50e9402f1cce04bb59186388869
Rasmus <rame0001@stud.kea.dk>
2025-01-30 21:01:46 +0100

    Jeg tror den er god nok

--- excercises/moms-beregner/java.js
+++ excercises/moms-beregner/java.js
@@ -1,5 +1,8 @@
-momsBeregner("beloeb","moms");
+document.addEventListener("DOMContentLoaded", function () {
+    momsBeregner();
+});
 
 function momsBeregner(beloeb, moms = 25) {
-console.log(`beløb inklusiv moms: ${beloeb} + ${25}%`);
+
+console.log(`beløb inklusiv moms: ${beloeb} + ${moms}%`);
 };
