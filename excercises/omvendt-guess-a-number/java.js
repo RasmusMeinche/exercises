3bfb54607edfb058f6dc4a2db2aa873448a9421d
Rasmus <rame0001@stud.kea.dk>
2025-02-02 23:24:13 +0100

    den virker

--- excercises/omvendt-guess-a-number/java.js
+++ excercises/omvendt-guess-a-number/java.js
@@ -1,28 +1,37 @@
-maxNum = 100;
-minNum = 0;
+let minNum = 1;
+let maxNum = 100;
+let answer = Math.floor((minNum + maxNum) / 2);
+console.log("Det her er answer:", answer)
 
-let answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-console.log("Tallet:", answer);
 
 document.addEventListener("DOMContentLoaded", function () {
-    indenSpil();
+    beforeGame();
+    document.querySelector("#text").textContent = "Tænk på et tal mellem 0 og 100";
 });
 
-function indenSpil() {
-    document.querySelector("#startSpillet").addEventListener("click", startSpillet);
+function beforeGame () {
+document.querySelector("#startSpillet").addEventListener("click", startGame)
+}
+
+function startGame() {
+    document.querySelector("#text").textContent = `Jeg gætter på ${answer}?`;
     document.querySelector("#forHoejt").addEventListener("click", forHoejt);
     document.querySelector("#forLavt").addEventListener("click", forLavt);
     document.querySelector("#perfekt").addEventListener("click", spilletSlutter);
-};
+}
 
-function forHoejt () {
+function forHoejt() {
+    maxNum = answer - 1;
+    answer = Math.floor((minNum + maxNum) / 2);
+    document.querySelector("#text").textContent = `Nemt, det sku da ${answer}?`;
+}
 
-    document.querySelector("#text").innerHTML = "Spil";
-};
-function forLavt () {
+function forLavt() {
+    minNum = answer + 1;
+    answer = Math.floor((minNum + maxNum) / 2);
+    document.querySelector("#text").textContent = `Nemt, det sku da ${answer}?`;
+}
 
-};
-
-function spilletSlutter () {
-
-};
+function spilletSlutter() {
+    document.querySelector("#text").textContent = "Det det nemmeste i verden";
+}
