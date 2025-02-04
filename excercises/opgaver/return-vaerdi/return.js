677ed0a969ecca12e79f9897e9443bf95e9ba6ab
RasmusMeinche <rame0001@stud.kea.dk>
2025-02-04 16:43:50 +0100

    mere java return

--- excercises/opgaver/return-vaerdi/return.js
+++ excercises/opgaver/return-vaerdi/return.js
@@ -8,8 +8,49 @@ console.log(greeting(firstName));
  */
 /* Opgave 2 */
 
-function greeting(firstName) {
+/* function greeting(firstName) {
+    return `Hello ${firstName}`;
+}
+const result = greeting("Any name");
+console.log(result); */
+
+/* opgave 3 */
+/* function greeting(firstName) {
+    return `Hello ${firstName}`;
+    console.log(greeting("any name"));
+}
+const result = greeting("Any name");
+ */
+
+/* opgave 4 */
+/* 
+function greeting() {
+    return `Hello ${firstName}`;
+}
+const result = greeting("Any name");
+console.log(result);
+ */
+
+/* opgave 5 */
+/* function greeting(firstName) {
     return `Hello ${firstName}`;
 }
 const result = greeting("Rasmus");
 console.log(result);
+
+const txt = `Greeting is ${greeting("a name")}`;
+console.log(txt); */
+
+/* opgave 6 */
+/* function greeting(firstName) {
+    return `Hello ${firstName}`;
+}
+const result = greeting("Any name"); */
+
+/* 
+function myFunc () {
+    let value;
+
+    return value;
+}
+ */
