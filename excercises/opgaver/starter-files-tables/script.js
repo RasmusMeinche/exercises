f2250ad934cddef8ae21073ad5f83118b288cf90
Rasmus <rame0001@stud.kea.dk>
2025-02-06 12:21:48 +0100

    Den rigtige java

--- excercises/opgaver/starter-files-tables/script.js
+++ excercises/opgaver/starter-files-tables/script.js
@@ -1,4 +1,4 @@
-const vehicles = [
+const veh = [
   { type: "Bus", fuel: "Diesel", passengers: 45, stops: ["Nørrebrogade", "Elmegade"] },
   { type: "Bil", fuel: "Benzin", passengers: 4, ownedBy: "Klaus" },
   { type: "Cykel", fuel: "Rugbrød", passengers: 0, ownedBy: "Jonas", isElectric: true },
@@ -11,18 +11,30 @@ const vehicles = [
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
 const tbodyPointer = document.querySelector("tbody");
-showTheseVehicles(vehicles);
+
+const allElectricVehicle = veh.filter((vehicle) => vehicle.isElectric === true);
+const moreSeats = veh.filter((vehicle) => vehicle.passengers === 2 || vehicle.passengers === 3 || vehicle.passengers === 4 || vehicle.passengers === 5);
+const ownerByJonas = veh.filter((vehicle) => vehicle.ownedBy === "Jonas");
+const allBreadFuel = veh.filter((vehicle) => vehicle.fuel === "Rugbrød" && vehicle.passengers === 2);
+
+function filterVehicles () {
+    if (allElectricVehicle === true) {
+        showTheseVehicles(allElectricVehicle);
+    }
+}
+
+showTheseVehicles(moreSeats);
 
 function showTheseVehicles(arr) {
-  arr.forEach((each) => {
-    tbodyPointer.innerHTML += `<tr>
-  <td>${each.type}</td>
-  <td>${each.fuel}</td>
-  <td>${each.passengers}</td> 
-  <td>${each.stops}</td>
-  <td>${each.ownedBy}</td>
-  <td>${each.isElectric}</td>
-  <td>${each.isTandem}</td>
-</tr>`;
-  });
+    arr.forEach(each => {
+        tbodyPointer.innerHTML += `<tr>
+        <td>${each.type}</td>
+        <td>${each.fuel}</td>
+        <td>${each.passengers}</td>
+        <td>${each.stops}</td>
+        <td>${each.ownedBy}</td>
+        <td>${each.isElectric}</td>
+        <td>${each.isTandem}</td>
+    </tr>`;
+    });
 }
