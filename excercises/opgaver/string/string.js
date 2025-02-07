17672137da77848118a510638b8a414e52412c96
Rasmus <rame0001@stud.kea.dk>
2025-02-07 15:10:54 +0100

    OPGAVER MED SUBSTRING OG CHART

--- excercises/opgaver/string/string.js
+++ excercises/opgaver/string/string.js
@@ -10,17 +10,34 @@ console.log(string); */
 
 console.log(navn.replaceAll(" ", "")); */
 
-const firstName = "Peter";
+
+/* const firstName = "Peter";
 const middleName = "Heronimous";
 const lastNameName = "Lind";
 
 const fullName = firstName + " " + middleName + " " + lastNameName;
-
-console.log(fullName[0]);
 
 splitName(fullName);
 
 function splitName(fullName) {
   let name = fullName.split(" ");
   console.log(name);
-}
+} */
+
+
+/* const fullName = "Peter Heronimous Lind";
+
+
+const name = fullName.split(" ");
+
+const firstName = name[0];
+const middleName = name[1];
+const lastNameName = name[2];
+
+console.log(name); */
+
+const fullName = "Peter";
+
+const nameUpper = fullName.substring(0, 2).toLowerCase() + fullName.charAt(2).toUpperCase() + fullName.substring(3).toLowerCase();
+
+console.log(nameUpper);
