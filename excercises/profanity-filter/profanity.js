57206c67ba44ce4a07e6e91850b5c41efbbc03b7
Rasmus <rame0001@stud.kea.dk>
2025-02-11 09:55:22 +0100

    Ganggang

--- excercises/profanity-filter/profanity.js
+++ excercises/profanity-filter/profanity.js
@@ -2,16 +2,24 @@ const curseWords = [  {    bad: "var",    good: "const",  },  {    bad: "float",
 
 console.log(curseWords);
 
+const dialog = document.querySelector("dialog");
 document.querySelector("button").addEventListener("click", switchWords);
+let timesClicked = 0;
+dialog.close();
 
 
 function switchWords() {
+    timesClicked ++;
     let textElement = document.querySelector("p");
     let text = textElement.textContent;
 
     curseWords.forEach((word) => {
         text = text.replace(word.bad, word.good);
     });
+
+    if (timesClicked >= 2) {
+        dialog.showModal();
+    }
 
     textElement.innerHTML = text;
 }
