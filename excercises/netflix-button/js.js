5aca3c1789a9af9c0bf6231a94671fc92e33747b
Rasmus <rame0001@stud.kea.dk>
2025-02-13 14:54:31 +0100

    Wizard of plugs

---
+++ excercises/netflix-button/js.js
@@ -0,0 +1,41 @@
+window.addEventListener("DOMContentLoaded", start);
+
+document.querySelector(".fill-button").classList.remove("fill-button");
+
+let buttonClicked = false;
+
+function start() {
+    const button = document.querySelector("button");
+    button.classList.add("fill-button");
+
+    button.addEventListener("animationend", animationEnded);
+    button.addEventListener("click", buttonHasBeenClicked);
+    button.addEventListener("mouseover", putOnAnimation);
+}
+
+function animationEnded() {    
+    if (buttonClicked === true) {
+        document.body.style.backgroundColor = "black";
+    } else {
+        document.body.style.backgroundColor = "black";
+    }
+    
+    document.querySelector("button").classList.remove("fill-button");
+    document.querySelector("button").classList.add("standard");
+    
+}
+
+function buttonHasBeenClicked() {
+    buttonClicked = true;
+    document.body.style.backgroundColor = "white";
+    document.querySelector("button").classList.remove("fill-button");
+    document.querySelector("button").classList.add("standard");
+    document.querySelector("button").style.color = "black";
+}
+
+function putOnAnimation() {
+    if (buttonClicked === true) {
+    } else {
+        start();
+    }
+}
