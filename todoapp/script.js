57355b957b8629a9c5c13d37abd2b64e329290ff
RasmusMeinche <rame0001@stud.kea.dk>
2025-02-17 22:38:16 +0100

    Bruh

--- todoapp/script.js
+++ todoapp/script.js
@@ -1,3 +1,28 @@
-const clone = document.querySelector("#tp").content.cloneNode(true);
+    window.addEventListener("DOMContentLoaded", start);
 
-clone.querySelector("[data-title=1]").textContent = "tp.title1";
+function start() {
+    document.querySelector("#add-knap").addEventListener("click", formComesUp);
+}
+
+function formComesUp() {
+    document.querySelector(".opgaver").classList.remove("none");
+}
+
+function openUp() {
+    const inputOverskrift = document.getElementById("input-overskrift");
+    const inputText = document.getElementById("input-text");
+
+    localStorage.setItem("valueOfOverskrift", inputOverskrift.value);
+    localStorage.setItem("valueOfText", inputText.value);
+
+    SavedData();
+}
+
+function SavedData() {
+    const clone = document.querySelector("#opgave-template").content.cloneNode(true);
+
+    clone.querySelector("[data-field=over]").textContent = localStorage.getItem("valueOfOverskrift");
+    clone.querySelector("[data-field=besk]").textContent = localStorage.getItem("valueOfText");
+
+    document.querySelector("#template-input").appendChild(clone);
+}
